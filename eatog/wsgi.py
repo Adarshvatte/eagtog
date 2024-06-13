b61fbf6e5fa40b1bb4583fe4fbb49a52e7b39cf0
Adarshvatte <169079568+Adarshvatte@users.noreply.github.com>
2024-06-13 20:11:50 +0530

    wsgi.py

--- eatog/wsgi.py
+++ eatog/wsgi.py
@@ -1,18 +1,19 @@
-"""
-WSGI config for eatog project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+import traceback
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eatog.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eatog.settings')
 
-application = get_wsgi_application()
-
-app = application
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    # Log the exception traceback
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    traceback_details = {
+        'type': exc_type.__name__,
+        'message': str(exc_value),
+        'traceback': traceback.format_tb(exc_traceback)
+    }
+    print("Exception in WSGI application:", traceback_details)
+    raise e
